4a7265b32c2598f678a08a22a753a8e63d44dc82
Sarah Allen <sarah@ultrasaurus.com>
2025-04-20 03:13:51 -0700

    fix typo

--- website/setup.sh
+++ website/setup.sh
@@ -1,4 +1,4 @@
-git clone --depth=1 -single-branch --branch=main git@github.com:ultrasaurus/altwebgen.git 
+git clone --depth=1 --single-branch --branch=main git@github.com:ultrasaurus/altwebgen.git 
 cd altwebgen
 cargo install --path ..
 cd ..
