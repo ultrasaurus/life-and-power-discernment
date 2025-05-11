aba86e962e31aabf80c69f5efc6e173bfea02bc4
Sarah Allen <sarah@ultrasaurus.com>
2025-05-10 18:35:19 -0700

    experimental alt pages deploy
    rename gh-pages to .pages, trying out cloudflare
    no prefix and baseurl needs to be empty
    use pacific timezone for build info
    change deploy action to only run on "github" branch (unused for now)

--- .pages/build.sh
+++ .pages/build.sh
@@ -1,19 +1,23 @@
 #! /bin/sh
 
-DEST=gh-pages/_website
+DEST=.pages/_website
 mkdir -p $DEST
 
 SOURCE="website/source"
 
-cp gh-pages/_site.yaml $SOURCE/.
+cp .pages/_site.yaml $SOURCE/.
 
 BUILD_FILE="$SOURCE/build.md"
 echo $BUILD_FILE
 
-DATE_STRING=$(date +"%a, %b %d %Y - %I:%M %p")
+DATE_STRING=$(TZ=America/Los_Angeles date +"%a, %b %d %Y - %I:%M %p")
 echo $DATE_STRING
 
 cat <<EOT >> $BUILD_FILE
+
+&nbsp;
+
+&nbsp;
 
 &nbsp;
 
@@ -29,4 +33,4 @@ EOT
 (cd website;
 echo ============ building website to ../$DEST;
 echo ============ `pwd`;
-altwebgen --prefix life-and-power-discernment -o ../$DEST build )
+altwebgen -o ../$DEST build )
